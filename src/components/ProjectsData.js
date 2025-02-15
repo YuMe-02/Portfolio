03aa095fb88563a02a56e8b232347b5fb949a6d2
YuMe-02 <darrenyu90@gmail.com>
2025-02-15 14:23:23 -0800

    more projects

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -2,7 +2,7 @@
 export const projectDetails = {
   healora: {
     id: "healora",
-    title: "HEALORA",
+    title: "AI Customer Churn",
     date: "Mar.2024",
     category: "Portfolio",
     type: "Design & Dev",
@@ -13,18 +13,43 @@ export const projectDetails = {
     projectUrl: "https://healora.com",
     githubUrl: "https://github.com/yourusername/healora"
   },  
-  emuni: {
-    id: "emuni",
-    title: "emuni",
-    date: "Feb.2024",
+  Diabetes: {
+    id: "Diabetes",
+    title: "Diabetes Predictave Model",
+    date: "Mar.2024",
     category: "Portfolio",
-    type: "Dev",
-    description: "Digital learning platform focusing on interactive educational experiences.",
-    skills: ["React", "Node.js", "MongoDB", "WebGL"],
-    imageUrl: "/path-to-emuni-image.jpg",
-    projectUrl: "https://emuni.edu",
-    githubUrl: "https://github.com/yourusername/emuni"
-  }
+    type: "Design & Dev",
+    description: "AI-powered healthcare platform designed to streamline patient care by integrating real-time health monitoring and virtual assistance.",
+    skills: ["Next.js", "Tailwind CSS", "Shadcn", "Framer Motion", "Hume AI", "Groq", "Joe", "Mama"],
+    imageUrl: "Architecture of the Gollins Melvin Ward Partnership - 1974.jpg",
+    alt:"Joe",
+    projectUrl: "https://healora.com",
+    githubUrl: "https://github.com/yourusername/healora"
+  },  
+  Hydro: {
+      id: "Hydro",
+      title: "Hydro Connect",
+      date: "Feb.2024",
+      category: "Portfolio",
+      type: "Dev",
+      description: "Digital learning platform focusing on interactive educational experiences.",
+      skills: ["React", "Node.js", "MongoDB", "WebGL"],
+      imageUrl: "./Hydro.jpg",
+      projectUrl: "https://emuni.edu",
+      githubUrl: "https://github.com/yourusername/emuni"
+    },
+  Proxy: {
+      id: "Proxy",
+      title: "Web Proxy",
+      date: "Feb.2024",
+      category: "Portfolio",
+      type: "Dev",
+      description: "Digital learning platform focusing on interactive educational experiences.",
+      skills: ["React", "Node.js", "MongoDB", "WebGL"],
+      imageUrl: "./Proxy.jpeg",
+      projectUrl: "https://emuni.edu",
+      githubUrl: "https://github.com/yourusername/emuni"
+    }
 };
 
 
