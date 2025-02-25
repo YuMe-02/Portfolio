cc2ecdee60685977355706857eb3cf9a04647957
YuMe-02 <darrenyu90@gmail.com>
2025-02-25 03:47:35 -0800

    desktop about fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,18 +2,17 @@ import React from "react";
 
 const About = () => {
   return (
-
-    <div className="about-heading"> <h1> About </h1> 
-
+    <div className="about-section">
+      <div className="about-heading">
+        <h1>About</h1>
+      </div>
 
       <div className="about-container">
-
-        <img src="pfp.jpg" alt="portrait"/>
+        <div className="about-image-container">
+          <img src="pfp.jpg" alt="portrait" />
+        </div>
 
         <div className="about-content">
-
-          
-
           <p>
             I'm a developer and designer based in San Francisco, blending my passion for technology with creative problem-solving. My journey began unexpectedly when a teapot mishap damaged my Nintendo DS, leading my uncle to teach me circuit board repair. That moment sparked a fascination that evolved into a deep interest in robotics and computer science.
           </p>
@@ -23,7 +22,7 @@ const About = () => {
           </p>
 
           <p>
-            Beyond tech, I’ve built a reputation as a freelance photographer and graphic designer, enhancing my approach to user experience and visual communication. When I'm not coding or designing, you'll find me capturing moments through photography, powerlifting, or recreating dishes from my favorite restaurants – always bringing the same creativity and attention to detail.
+            Currently, I am seeking Frontend, Full Stack, or Software Engineering roles.
           </p>
         </div>
       </div>
@@ -31,14 +30,4 @@ const About = () => {
   );
 };
 
-
 export default About;
-
-// "A subject holds no meaning without its context—its existence is shaped by the world around it."
-
-// https://calendar.ucsc.edu/event/santa_cruz_launchpad_2023
-
-// https://engineering.ucsc.edu/
-
-// https://www.cied.ucsc.edu/launchpad-2024
-
