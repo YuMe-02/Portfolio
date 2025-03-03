7d3a7b250f1eeb31d953398344b6ff9729c1c74e
YuMe-02 <darrenyu90@gmail.com>
2025-03-03 01:51:46 -0800

    home alignment

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const Home = () => (
           <h1 class="name animate delay-2">Darren Yu</h1>
           <p class="title animate delay-2">Developer &amp; Designer</p>
           <div class="social-links animate delay-3">
-              <a href="#" class="social-icon">
+              <a href="mailto:darrenyu90@gmail.com" class="social-icon">
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                       <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                       <polyline points="22,6 12,13 2,6"></polyline>
