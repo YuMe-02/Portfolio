ac0bfa88791a368e687a5e778856150cac647128
YuMe-02 <darrenyu90@gmail.com>
2025-03-06 03:06:53 -0800

    fixing images

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const Home = () => (
   <div className="main-content">
     <div className="center-content">
       <div className="profile-image animate delay-1">
-        <img src= "cover.png" alt="Darren Yu" />
+      <img src={`${process.env.PUBLIC_URL}/cover.png`} alt="cover" />
       </div>
       <div className="bio">
         <h1 className="name animate delay-2">Darren Yu</h1>
